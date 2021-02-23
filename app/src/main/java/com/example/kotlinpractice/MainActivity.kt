3c6971140bd7afe3a61814b44f961cca3e0aa23d
vbabiryek <vbabiryek@gmail.com>
2021-02-23 00:30:35 -0500

    intent extra added

--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.ButtonBarLayout
+import androidx.core.os.bundleOf
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,22 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myBtn = findViewById<Button>(R.id.mybutton);
-        val myTxtView = findViewById<TextView>(R.id.textView);
+        val myBtn = findViewById<Button>(R.id.mybutton)
+        val myTxtView = findViewById<TextView>(R.id.textView)
 
-        var timesClicked = 0;
+        var timesClicked = 0
 
         myBtn.setOnClickListener {
-            Toast.makeText(this, "Click me 10 times!", Toast.LENGTH_SHORT).show();
-            timesClicked += 1;
-            myTxtView.text = timesClicked.toString();
+            Toast.makeText(this, "Click me 10 times!", Toast.LENGTH_SHORT).show()
+            timesClicked += 1
+            myTxtView.text = timesClicked.toString()
             if (timesClicked == 10) {
-                Toast.makeText(this, "You may enter!", Toast.LENGTH_SHORT).show();
-                val myIntent = Intent(this,SecondActivity::class.java);
-                myIntent.putExtra("timesClicked", timesClicked);
-                startActivity(myIntent);
+                val myIntent = Intent(this,SecondActivity::class.java)
+                myIntent.putExtra("timesClicked", timesClicked.toString())
+                println("intent extra passed on here is: " + myIntent.getStringExtra("timesClicked"))
+                startActivity(myIntent)
             }
         }
-
     }
 }
--- app/src/main/java/com/example/kotlinpractice/SecondActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/SecondActivity.kt
@@ -1,24 +1,40 @@
 package com.example.kotlinpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val myWelcome = findViewById<TextView>(R.id.welcome);
-        val myCounter = findViewById<TextView>(R.id.counterTv);
-        val myFragBtn = findViewById<Button>(R.id.openFragBtn);
+        val myCounter = findViewById<TextView>(R.id.counterTv)
+        val myFragBtn = findViewById<Button>(R.id.openFragBtn)
 
-        //We need to grab our current count from our extras in the intent that got us here
-        //We need to set up another onClick listener for a count up to 20
-        //Then we need to create a fragment that makes a network call to some API to return a recyclerview of those items
-        //Then upon pressing the button up to 20 count, it opens the fragment and we have out list to view
-        //Then a button that takes us to a third activity that holds a viewpager created in mvvm architecture
+        val intentStr: Intent = intent
+        var counterString = intentStr.getStringExtra("timesClicked")
+        println("intent extra is: " + intentStr.getStringExtra("timesClicked"))
+        var timesClicked = Integer.valueOf(counterString.toString())
+        myCounter.text = counterString
+
+        Toast.makeText(this, "Hit the button 10 more times", Toast.LENGTH_LONG).show()
+
+        myFragBtn.setOnClickListener {
+            myCounter.text = timesClicked.toString()
+            timesClicked += 1
+            if(timesClicked == 20){
+                Toast.makeText(this, "You've hit 20!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Create a fragment that makes a network call to some API to return a recyclerview of those items
+        //Upon pressing the button up to 20 count, it opens the fragment and we have out list to view
+        //Create a button that takes us to a third activity that holds a viewpager - MVVM arch
 
     }
 }
